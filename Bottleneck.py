fdf82ab397f43fb9c87e22044a22438939478e5d
LalitSandeep <36740029+LalitSandeep@users.noreply.github.com>
2018-06-09 05:45:53 -0400

    Update Bottleneck.py

--- Bottleneck.py
+++ Bottleneck.py
@@ -1,9 +1,4 @@
 
-# coding: utf-8
-
-# In[1]:
-
-
 get_ipython().magic('matplotlib inline')
 
 import numpy as np
@@ -18,27 +13,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set()
-
-
-# In[2]:
-
-
-# Dimensions of our images.
 img_width, img_height = 224, 224
 
 n_classes = 128
 train_data_dir = 'train_chunk1'
 validation_data_dir = 'valid'
 
-
-# In[3]:
-
-
-# Pixel values rescaling from [0, 255] to [0, 1] interval
 datagen = ImageDataGenerator(rescale=1./255)
 batch_size = 32
 
-# Retrieve images and their classes for train and validation sets
 train_generator_bottleneck = datagen.flow_from_directory(
         train_data_dir,
         target_size=(img_width, img_height),
@@ -56,32 +39,16 @@ validation_generator_bottleneck = datagen.flow_from_directory(
 nb_train_samples = len(train_generator_bottleneck.filenames)  
 print(nb_train_samples)
 
-
-# In[4]:
-
-
 model_vgg = applications.VGG16(include_top=False, weights='imagenet')
-
-
-# In[5]:
-
 
 import math
 predict_size_train = int(math.ceil(nb_train_samples / batch_size))  
 bottleneck_features_train = model_vgg.predict_generator(train_generator_bottleneck,predict_size_train) 
 np.save(open('bottleneck_features_train.npy', 'wb'), bottleneck_features_train)
 
-
-# In[6]:
-
-
 predict_size_valid = int(math.ceil(6309 / batch_size))  
 bottleneck_features_validation = model_vgg.predict_generator(validation_generator_bottleneck,predict_size_valid)
 np.save(open('bottleneck_features_validation.npy', 'wb'), bottleneck_features_validation)
-
-
-# In[7]:
-
 
 num_classes = len(train_generator_bottleneck.class_indices)
 
@@ -91,15 +58,7 @@ train_labels = to_categorical(train_generator_bottleneck.classes, num_classes=nu
 validation_data = np.load(open('bottleneck_features_validation.npy', 'rb'))
 validation_labels = to_categorical(validation_generator_bottleneck.classes, num_classes=num_classes)
 
-
-# In[19]:
-
-
 print(type(train_data))
-
-
-# In[8]:
-
 
 model_top = Sequential()
 model_top.add(Flatten(input_shape=train_data.shape[1:]))
@@ -107,29 +66,16 @@ model_top.add(Dense(512, activation='relu'))
 model_top.add(Dropout(0.3))
 model_top.add(Dense(n_classes, activation='softmax'))
 
-
-# In[9]:
-
-
 model_top.compile(loss='categorical_crossentropy',
                   optimizer='rmsprop',
                   metrics=['accuracy'])
-
-
-# In[10]:
-
 
 epochs = 100
 train_samples = 164171
 validation_samples = 6309
 
-
-# In[11]:
-
-
 checkpointer = ModelCheckpoint(filepath='bottleneck_features.h5', monitor='val_acc', verbose=1, save_best_only=True)
 
-# Early stopping
 early_stopping = EarlyStopping(monitor='val_acc', verbose=1, patience=5)
 
 history = model_top.fit(
@@ -142,14 +88,7 @@ history = model_top.fit(
         validation_data=(validation_data, validation_labels))
 
 
-# In[12]:
-
-
 model_top.evaluate(validation_data, validation_labels)
-
-
-# In[13]:
-
 
 fig, ax = plt.subplots()
 plt.plot(history.history['acc'])
@@ -160,10 +99,6 @@ plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
 fig.savefig('bottleneck_features.svg', format='svg', dpi=1200)
-
-
-# In[14]:
-
 
 with open('bottleneck_features.json', 'w') as f:
     f.write(model_top.to_json())

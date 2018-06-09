58da3a959d081bd4b4690e267bf050bfff19a132
LalitSandeep <36740029+LalitSandeep@users.noreply.github.com>
2018-06-09 05:52:46 -0400

    Update basic_cnn (1).py

--- basic_cnn (1).py
+++ basic_cnn (1).py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 get_ipython().magic('matplotlib inline')
 
 import numpy as np
@@ -16,26 +10,13 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set()
-
-
-# In[2]:
-
-
 img_width, img_height = 224, 224
-
 n_classes = 128
 train_data_dir = 'train_chunk1'
 validation_data_dir = 'valid'
 
-
-
-# In[3]:
-
-
 datagen = ImageDataGenerator(rescale=1. / 255)
 batch_size = 32
-
-# Retrieve images and their classes for train and validation sets
 train_generator = datagen.flow_from_directory(
         train_data_dir,
         target_size=(img_width, img_height),
@@ -45,11 +26,6 @@ validation_generator = datagen.flow_from_directory(
         validation_data_dir,
         target_size=(img_width, img_height),
         batch_size=batch_size)
-
-
-# In[4]:
-
-
 
 model = Sequential()
 model.add(Convolution2D(32, (3, 3), input_shape=(img_width, img_height, 3)))
@@ -70,28 +46,14 @@ model.add(Activation('relu'))
 model.add(Dropout(0.3))
 model.add(Dense(n_classes))
 model.add(Activation('softmax'))
-
-
-# In[5]:
-
-
 model.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
-
-
-# In[6]:
-
 
 epochs = 100
 train_samples = 164171
 validation_samples = 6309
 
-
-# In[7]:
-
-
-# Model saving callback
 checkpointer = ModelCheckpoint(filepath='basic_cnn.h5', monitor='val_acc', verbose=1, save_best_only=True)
 
 # Early stopping
@@ -106,15 +68,7 @@ history = model.fit_generator(
         validation_data=validation_generator,
         validation_steps=validation_samples // batch_size,)
 
-
-# In[8]:
-
-
 model.evaluate_generator(validation_generator, validation_samples, workers=12)
-
-
-# In[9]:
-
 
 fig, ax = plt.subplots()
 plt.plot(history.history['acc'])
@@ -125,10 +79,6 @@ plt.xlabel('Epoch')
 plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
 fig.savefig('basic_cnn.svg', format='svg', dpi=1200)
-
-
-# In[ ]:
-
 
 with open('basic_cnn.json', 'w') as f:
     f.write(model.to_json())
